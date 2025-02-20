3eaad12a433e71cafb5ced544a5e943a3addc418
Hari <>
2025-02-20 14:05:11 +0530

    Same as changes

---
+++ src/componets/NavBar.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+const Navbar = () => {
+    return (
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+            <div className="container">
+                <a className="navbar-brand" href="#">
+                    Radhesh Reddy.G
+                </a>
+                <button
+                    className="navbar-toggler"
+                    type="button"
+                    data-bs-toggle="collapse"
+                    data-bs-target="#navbarNav"
+                    aria-controls="navbarNav"
+                    aria-expanded="false"
+                    aria-label="Toggle navigation"
+                >
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarNav">
+                    <ul className="navbar-nav ms-auto">
+
+                        <li className="nav-item">
+                            <a className="nav-link" href="#about">
+                                About Me
+                            </a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#services">
+                                Services
+                            </a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#contact">
+                                Contact @ radhesh
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    );
+};
+
+export default Navbar;
